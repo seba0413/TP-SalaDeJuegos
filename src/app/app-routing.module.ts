02ffd3fdb8f84b01da50aeedd92df02b6f5c6db5
seba0413 <sebastianlpasquini@gmail.com>
2019-09-10 21:59:30 -0300

    primer juego (adivina el numero) funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PrincipalComponent } from './componentes/principal/principal.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { JuegosComponent } from './componentes/juegos/juegos.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { AdivinaElNumeroComponent } from './componentes/adivina-el-numero/adivina-el-numero.component';
+
 
 
 const routes: Routes = [
-  {path: '' , component: PrincipalComponent}
+  { path: '' , component: PrincipalComponent },
+  { path: 'Principal' , component: PrincipalComponent },
+  { path: 'Juegos' , component: JuegosComponent, children:
+    [
+       { path: 'Adivina' , component: AdivinaElNumeroComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './componentes/principal/principal.component';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { JuegosComponent } from './componentes/juegos/juegos.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { AdivinaElNumeroComponent } from './componentes/adivina-el-numero/adivina-el-numero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    JuegosComponent,
+    MenuComponent,
+    AdivinaElNumeroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
